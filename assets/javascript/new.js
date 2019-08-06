013e176cc434e866ed71dc249dc0cecc06d07891
Michal Terranova <50525855+mrterranova@users.noreply.github.com>
2019-08-06 16:32:30 -0400

    Update new.js

--- assets/javascript/new.js
+++ assets/javascript/new.js
@@ -157,7 +157,7 @@ function roundComplete(){
             'volume':0.4,
             'autoplay':'autoplay'
         }).appendTo("body");
-        wrong =[];
+        wrong =[""];
         startGame();
 
     }
